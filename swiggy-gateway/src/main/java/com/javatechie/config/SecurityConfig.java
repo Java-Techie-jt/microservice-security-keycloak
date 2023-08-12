6507fcfbf75dabefe19846f1ce56b0d6de6cc796
Basant kumar Hota <basahota@users.noreply.github.com>
2023-08-12 15:31:16 +0530

    Add files via upload

---
+++ swiggy-gateway/src/main/java/com/javatechie/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.javatechie.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity serverHttpSecurity) {
+        return serverHttpSecurity.csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchange -> exchange.pathMatchers("/eureka/**")
+                        .permitAll()
+                        .anyExchange().authenticated()
+                ).oauth2ResourceServer((oauth) -> oauth
+                        .jwt(Customizer.withDefaults()))
+                .build();
+    }
+}
